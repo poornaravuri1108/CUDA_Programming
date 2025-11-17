6c331ad51b25bdcec6abffa11d82c7549fdeb367
poornaravuri1108 <poornaravuri4420@gmail.com>
2025-11-17 09:17:54 -0800

    increasing grid size

--- 1DConv/naive.cu
+++ 1DConv/naive.cu
@@ -18,7 +18,7 @@ __global__ void conv_1d_kernel(const float* __restrict__ input, const float* __r
 
 extern "C" void solve(const float* input, const float* kernel, float* output, int input_size, int kernel_size){
 	int output_size = input_size-kernel_size+1;
-	int threadsPerBlock = 256;
+	int threadsPerBlock = 512;
 	int blocksPerGrid = (output_size+threadsPerBlock-1)/threadsPerBlock;
 
 	conv_1d_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, kernel, output, input_size, kernel_size);
